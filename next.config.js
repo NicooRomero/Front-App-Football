4fdd6c7810b4a5c5fb83cc37cc39c128a6d0c277
Nicolas Romero <70491068+NicooRomero@users.noreply.github.com>
2023-12-07 11:34:08 -0300

    fix images load

--- next.config.js
+++ next.config.js
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'http',
+        hostname: 'serveranon.ddns.net',
+        port: '3000'
+      },
+    ],
+  },
+}
 
 module.exports = nextConfig
